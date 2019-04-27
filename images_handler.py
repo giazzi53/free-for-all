4306bf31fc9c2d9dd5e6202f41525015b0bfd0cf
Matheus Lança <matheus.7910@hotmail.com>
2019-04-27 12:49:48 -0300

    [LANCA] - Ajustando erro do images_handler

--- images_handler.py
+++ images_handler.py
@@ -3,12 +3,12 @@ import pygame
 class Images():
 
     def __init__(self):
-        self.joaoLourao = [pygame.image.load('Images/Enemies/joaoLourao-ringue.png'), (60, 60)) for i in range(10)],
-        self.mikeTyson = [pygame.image.load('Images/Enemies/mikeTyson-ringue.png'), (60, 60)) for i in range(10)],
-        self.cobra = [pygame.image.load('Images/Enemies/cobra-floresta.png'), (60, 60)) for i in range(10)],
-        self.onca = [pygame.image.load('Images/Enemies/onca-floresta.png'), (60, 60)) for i in range(10)],
-        self.scorpion = [pygame.image.load('Images/Enemies/scorpion-deserto.png'), (60, 60)) for i in range(10)],
-        self.mumia = [pygame.image.load('Images/Enemies/mumia-deserto.png'), (60, 60)) for i in range(10)]
+        self.joaoLourao = [pygame.image.load('Images/Enemies/joaoLourao-ringue.png') for i in range(10)],
+        self.mikeTyson = [pygame.image.load('Images/Enemies/mikeTyson-ringue.png') for i in range(10)],
+        self.cobra = [pygame.image.load('Images/Enemies/cobra-floresta.png') for i in range(10)],
+        self.onca = [pygame.image.load('Images/Enemies/onca-floresta.png') for i in range(10)],
+        self.scorpion = [pygame.image.load('Images/Enemies/scorpion-deserto.png') for i in range(10)],
+        self.mumia = [pygame.image.load('Images/Enemies/mumia-deserto.png') for i in range(10)]
 
     def getJoaoLourao(self):
         return self.joaoLourao

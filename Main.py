1670f4bd2a3f0c6d3bcc000d801a14694f489c87
Matheus Lança <matheus.7910@hotmail.com>
2019-04-29 19:40:48 -0300

    Criando nova branch, com features adicionais

---
+++ Main.py
@@ -0,0 +1,71 @@
+import pygame
+import time
+import Ranking
+from maps import Map
+
+Ranking.SetUsername()
+
+pygame.init()
+pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=256)
+pygame.mixer.music.load('audio/background_music.mp3')
+pygame.mixer.music.set_volume(0.1)
+pygame.mixer.music.play(-1)
+
+game_map = Map()
+
+##poring_right_image = [pygame.image.load('sprites_monsters/poring/right/frame_' + str(i) + '_delay-0.1s.png') for i in range(10)]
+##poring_left_image = [pygame.image.load('sprites_monsters/poring/left/frame_' + str(i) + '_delay-0.1s.png') for i in range(10)]
+##
+##angeling_right_images = [pygame.image.load('sprites_allies/angeling/right/frame_' + str(i) + '_delay-0.15s.png') for i in range(27)]
+##angeling_left_images = [pygame.image.load('sprites_allies/angeling/left/frame_' + str(i) + '_delay-0.15s.png') for i in range(27)]
+
+game_map.spawnAllies(1, game_map.images.getAngelingImages(), 1)
+game_map.spawnMonsters(1, game_map.images.getPoringImages(), 1, False)
+
+#image = pygame.image.load('sprites_player/sprite' + str(player1.passo) + '_player_' + str(player1.grau) + '.png')
+
+
+while not game_map.windowClosed:
+    pygame.time.Clock().tick(30)
+    game_map.screen.fill((255, 255, 255))
+
+    if game_map.initialScreen:
+        game_map.initalScreen()
+    elif game_map.inGame:
+        game_map.blitBackgroundMap()    
+        
+        game_map.player.animatePlayerSprite()
+
+        game_map.shotsInteractions()
+
+        game_map.bulletsInteractions()
+        
+        game_map.monstersInteractions()
+                        
+        game_map.alliesInteractions()
+                        
+        game_map.checkEndOfLevel()
+
+        game_map.showPlayerInfos()
+
+        game_map.showGuiLevelMap()
+
+        pygame.display.update()
+    else:
+        game_map.endOfGame()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_map.inGame = False
+                game_map.windowClosed = True
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                if(pos[0] >= 380 and pos[0] <= 600 and pos[1] >= 630 and pos[1] <= 690):
+                    game_map = Map()
+                    game_map.spawnAllies(1, game_map.images.getAngelingImages(), 1)
+                    game_map.spawnMonsters(1, game_map.images.getPoringImages(), 1, False)
+
+    game_map.checkEvents()
+
+    
---
+++ shots.py
@@ -0,0 +1,25 @@
+import pygame
+
+class Shot():
+
+    def __init__(self, position, eixo, vel):
+        self.position = position
+        self.eixo = eixo
+        if eixo == 'x':
+            self.img = pygame.image.load('sprites_player/shotX.png')
+        else:
+            self.img = pygame.image.load('sprites_player/shotY.png')
+        
+        self.rect = pygame.Rect(position[0], position[1], self.img.get_width(), self.img.get_height())
+        self.vel = vel * 30
+
+    def move(self):
+        if self.eixo == 'x':
+            self.position = (self.position[0]+self.vel, self.position[1])
+            self.rect.x += self.vel
+        else:
+            self.position = (self.position[0], self.position[1]+self.vel)
+            self.rect.y += self.vel
+
+    def is_collided_with(self, sprite):
+        return self.rect.colliderect(sprite.rect)
